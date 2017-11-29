69a3fa197b2edfa82e2024223e227412b04505de
marizaswart <mariza.swart@gmail.com>
2017-11-29 20:10:15 +0100

    Create new article with title, content and button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   # get 'landing/index'
 
   root controller: :landing, action: :index
+  resources :articles
 end
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -11,3 +11,19 @@ Given("the following articles exists") do |table|
     FactoryBot.create(:article, article)
   end
 end
+
+When("I click {string} link") do |link|
+  click_link link
+  end
+
+When("I fill in {string} with {string}") do |field_name, content|
+  fill_in field_name, with: content
+end
+
+When("I click {string} button") do |button|
+  click_button button
+end
+
+Then("I should be on {string} page") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
