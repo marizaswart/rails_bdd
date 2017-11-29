f41458952eb320415592090e7dbe290e00ef3a7b
marizaswart <mariza.swart@gmail.com>
2017-11-29 22:17:35 +0100

    Set up create an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,17 @@
 class ArticlesController < ApplicationController
   def new
-
+    @article = Article.new
   end
 
   def create
+    article = Article.new
+    article.title = params[:article][:title]
+    article.content = params[:article][:content]
+    article.save
+    redirect_to article
+  end
+
+  def show
 
   end
 end
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -14,7 +14,7 @@ end
 
 When("I click {string} link") do |link|
   click_link link
-  end
+end
 
 When("I fill in {string} with {string}") do |field_name, content|
   fill_in field_name, with: content
@@ -24,6 +24,7 @@ When("I click {string} button") do |button|
   click_button button
 end
 
-Then("I should be on {string} page") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Then("I should be on {string} page") do |article_title|
+  article = Article.find_by(title: article_title)
+  expect(page.current_path).to eq "/articles/#{article.id}"
 end
