f76fee9ba6fea262721f89fe78ec0b6b0f365e15
marizaswart <mariza.swart@gmail.com>
2017-12-03 18:31:09 +0100

    Create method to permit params for title and content

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,15 +4,21 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    article = Article.new
-    article.title = params[:article][:title]
-    article.content = params[:article][:content]
-    article.save
+    article = Article.create(article_params)
+    # article.title = params[:article][:title]
+    # article.content = params[:article][:content]
+    # article.save
     flash[:notice] = "Article was successfully created."
     redirect_to article
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params[:article].permit(:title, :content)
   end
 end
