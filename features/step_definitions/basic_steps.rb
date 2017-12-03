3ed4b47a2a2021553d7fd78841fdfcc3dc625268
marizaswart <mariza.swart@gmail.com>
2017-12-03 20:30:50 +0100

    Update master with correct steps

---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,34 @@
+Given('I visit the landing page') do
+  visit '/'
+end
+
+Then("I should see {string}") do |content|
+  expect(page).to have_content content
+end
+
+Given("the following articles exists") do |table|
+  table.hashes.each do |article|
+    FactoryBot.create(:article, article)
+  end
+end
+
+When("I click {string} link") do |link|
+  click_link link
+end
+
+When("I fill in {string} with {string}") do |field_name, content|
+  fill_in field_name, with: content
+end
+
+When("I click {string} button") do |button|
+  click_button button
+end
+
+Then("I should be on {string} page") do |article_title|
+  article = Article.find_by(title: article_title)
+  expect(page.current_path).to eq "/articles/#{article.id}"
+end
+
+Then("I should not see {string}") do |content|
+  expect(page).to have_no_content content
+end
